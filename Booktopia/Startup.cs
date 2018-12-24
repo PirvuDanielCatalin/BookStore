91b3227858a81cb14e4862b3d8875d591d05ed0e
Radu Sorin <info_eu@yahoo.ro>
2018-12-24 13:18:06 +0200

    Create project

---
+++ Booktopia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Booktopia.Startup))]
+namespace Booktopia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
